2e5b3f578a25772454ba1bcb15f6b19387ab58e6
aikiriao <samuraiaiki@gmail.com>
2021-05-31 00:47:52 +0900

    change directory structure.

--- slide/figs/make_howto_2dfwt.py
+++ slide/figs/make_howto_2dfwt.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../implement'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../implementation'))
 import fwt
 import numpy as np
 
