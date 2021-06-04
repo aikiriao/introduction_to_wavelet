bfe3d27bd7297e4115c6f94c00df6fabde1933f5
aikiriao <samuraiaiki@gmail.com>
2021-06-04 09:02:38 +0900

    replace some graphs, add URL, fix some expressions.

--- slide/figs/plot_haar_wavelets.sh
+++ slide/figs/plot_haar_wavelets.sh
@@ -32,15 +32,15 @@ gnuplot << EOF
     set yrange [-2.1:2.1]
     set output "haar_wavelets.pdf"
     plot haar(x, 1, 0) t "a=1, b=0" linewidth 5 linetype rgb "black", \
-        haar(x, 0.25, 3) t "a=0.25, b=3" linewidth 5 linetype rgb "red", \
-        haar(x, 4, 5) t "a=4, b=5" linewidth 5 linetype rgb "blue"
+        haar(x, 0.25, 3) t "a=0.25, b=3" linewidth 5 linetype rgb "red" dashtype '_', \
+        haar(x, 4, 5) t "a=4, b=5" linewidth 5 linetype rgb "blue" dashtype '.'
 
     set ylabel 'phi(t)'
     set xrange [-1:10]
     set yrange [-0.1:2.1]
     set output "haar_scaling_functions.pdf"
     plot haar_scaling_discrete(x, 0, 0) t "m=0, n=0" linewidth 5 linetype rgb "black", \
-        haar_scaling_discrete(x, 2, 8) t "m=2, n=8" linewidth 5 linetype rgb "red", \
-        haar_scaling_discrete(x, -2, 1) t "m=-2, n=1" linewidth 5 linetype rgb "blue"
+        haar_scaling_discrete(x, 2, 8) t "m=2, n=8" linewidth 5 linetype rgb "red" dashtype '_', \
+        haar_scaling_discrete(x, -2, 1) t "m=-2, n=1" linewidth 5 linetype rgb "blue" dashtype '.'
 
 EOF
